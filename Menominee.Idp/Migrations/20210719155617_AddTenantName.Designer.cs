d349a0736059555464473d8fbab27c097684772e
David Eaton <davide@stocktrac.com>
2021-07-19 12:55:06 -0400

    Add TenantName to ApplicationUser

--- Menominee.Idp/Migrations/20210719155617_AddTenantName.Designer.cs
+++ Menominee.Idp/Migrations/20210719155617_AddTenantName.Designer.cs
@@ -4,14 +4,16 @@ using Menominee.Idp.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Menominee.Idp.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210719155617_AddTenantName")]
+    partial class AddTenantName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
