6377a24f8c42f84cbfb7f487fdeb223d1f251106
David Eaton <davide@stocktrac.com>
2021-08-20 10:21:06 -0400

    IDP: Enforce Tenant Name uniqueness in AspNetTenant
    Add alternate key to AspNetTenant in UserDbContext.OnModelCreating()
    Add database migration
    Update database

---
+++ Menominee.Idp/Migrations/20210820134229_AddAspNetTenantAlternateKey_Name.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Menominee.Idp.Migrations
+{
+    public partial class AddAspNetTenantAlternateKey_Name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AlternateKey_Name",
+                table: "AspNetTenants",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AlternateKey_Name",
+                table: "AspNetTenants");
+        }
+    }
+}
