2775e2e56c9414a83db73b045cac8395f05cbce2
David Eaton <davidzaeaton@outlook.com>
2021-11-10 17:26:19 -0500

    Add AuthService to secure client

---
+++ Menominee.Client.Secure/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using CustomerVehicleManagement.Shared.Models;
+using System.Threading.Tasks;
+
+namespace Menominee.Client.Secure.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+        Task<RegisterUserResult> Register(RegisterUser registerModel);
+    }
+}
