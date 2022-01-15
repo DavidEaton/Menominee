195b2b4d2b1924da902bb36adc5c7f44a0a23019
David Eaton <davidzaeaton@outlook.com>
2022-01-15 12:20:34 -0500

    IDP: IdentityHostingStartup: Build environment-specific connection string Add app settings

--- Menominee.Idp/Areas/Identity/IdentityHostingStartup.cs
+++ Menominee.Idp/Areas/Identity/IdentityHostingStartup.cs
@@ -2,28 +2,52 @@
 using Menominee.Idp.Data.Contexts;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 [assembly: HostingStartup(typeof(Menominee.Idp.Areas.Identity.IdentityHostingStartup))]
 namespace Menominee.Idp.Areas.Identity
 {
     public class IdentityHostingStartup : IHostingStartup
     {
+        private string Connection;
+
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) =>
             {
+                var Environment = context.Configuration.GetValue<string>("ApplicationSettings:Environment");
+
+                if (Environment == "Development")
+                    Connection = context.Configuration.GetConnectionString("UserDbContextConnection");
+
+                if (Environment == "Production")
+                {
+                    var builder = new SqlConnectionStringBuilder
+                    {
+                        DataSource = context.Configuration.GetValue<string>("Database:DataSource"),
+                        UserID = context.Configuration.GetValue<string>("Database:UserId"),
+                        Password = context.Configuration.GetValue<string>("Database:Password"),
+                        InitialCatalog = context.Configuration.GetValue<string>("Database:InitialCatalog"),
+                    };
+
+                    Connection = new SqlConnection(builder.ConnectionString).ConnectionString;
+                }
+
+                Log.Logger.Information($"Environment: {Environment}");
+
+                Log.Logger.Information($"MinimumPasswordLength: {int.Parse(context.Configuration.GetValue<string>("ApplicationSettings:MinimumPasswordLength"))}");
+
                 services.AddDbContext<UserDbContext>(options =>
-                    options.UseSqlServer(
-                         context.Configuration
-                        .GetConnectionString("UserDbContextConnection")));
+                    options.UseSqlServer(Connection));
 
                 services.AddIdentity<ApplicationUser, IdentityRole>(
                          options =>
                          {
-                             options.Password.RequiredLength = 8;
+                             options.Password.RequiredLength = int.Parse(context.Configuration.GetValue<string>("ApplicationSettings:MinimumPasswordLength"));
                              options.SignIn.RequireConfirmedAccount = true;
                          })
                         .AddEntityFrameworkStores<UserDbContext>()
