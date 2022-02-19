3b42abf7d5ccd07b33190cbe8766022281e263a5
David Eaton <davidzaeaton@outlook.com>
2022-02-19 10:33:30 -0500

    Resolve breaking changes after upgrading Telerik UI for Blazor Fix PersonToRead, CustomerToRead Person.Name

--- CustomerVehicleManagement.Shared/Models/PersonToRead.cs
+++ CustomerVehicleManagement.Shared/Models/PersonToRead.cs
@@ -9,13 +9,32 @@ namespace CustomerVehicleManagement.Shared.Models
     public class PersonToRead
     {
         public long Id { get; set; }
-        public PersonName Name { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
         public Gender Gender { get; set; }
         public DateTime? Birthday { get; set; }
         public DriversLicenseToRead DriversLicense { get; set; }
         public AddressToRead Address { get; set; }
         public IReadOnlyList<PhoneToRead> Phones { get; set; } = new List<PhoneToRead>();
         public IReadOnlyList<EmailToRead> Emails { get; set; } = new List<EmailToRead>();
+        public string LastFirstMiddle
+        {
+            get => string.IsNullOrWhiteSpace(MiddleName) ? $"{LastName}, {FirstName}" : $"{LastName}, {FirstName} {MiddleName}";
+        }
+        public string LastFirstMiddleInitial
+        {
+            get => string.IsNullOrWhiteSpace(MiddleName) ? $"{LastName}, {FirstName}" : $"{LastName}, {FirstName} {MiddleName[0]}.";
+        }
+        public string FirstMiddleLast
+        {
+            get => string.IsNullOrWhiteSpace(MiddleName) ? $"{FirstName} {LastName}" : $"{FirstName} {MiddleName} {LastName}";
+        }
+
+        public override string ToString()
+        {
+            return LastFirstMiddleInitial;
+        }
 
         public static PersonToRead ConvertToDto(Person person)
         {
@@ -23,7 +42,9 @@ namespace CustomerVehicleManagement.Shared.Models
                 ? new PersonToRead()
                 {
                     Id = person.Id,
-                    Name = person.Name,
+                    FirstName = person.Name.FirstName,
+                    MiddleName = person.Name.MiddleName,
+                    LastName = person.Name.LastName,
                     DriversLicense = DriversLicenseToRead.ConvertToDto(person.DriversLicense),
                     Address = person?.Address != null
                         ? AddressToRead.ConvertToDto(person.Address)
