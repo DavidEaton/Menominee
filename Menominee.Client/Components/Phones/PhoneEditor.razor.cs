5141314474d3ed6cfff4172336fe69e03df1ef77
Alan Craker <alanc@stocktrac.com>
2022-12-22 15:02:25 -0500

    Add phones and emails to vendor editor

---
+++ Menominee.Client/Components/Phones/PhoneEditor.razor.cs
@@ -0,0 +1,63 @@
+﻿using CustomerVehicleManagement.Shared.Models.Contactable;
+using Menominee.Client.Shared;
+using Menominee.Common.Enums;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Menominee.Client.Components.Phones
+{
+    public partial class PhoneEditor
+    {
+        [Inject]
+        IJSRuntime JsInterop { get; set; }
+
+        [Parameter]
+        public PhoneToWrite Phone { get; set; }
+
+        [Parameter]
+        public FormMode FormMode { get; set; } = FormMode.Unknown;
+
+        [Parameter]
+        public EventCallback OnSave { get; set; }
+
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+
+        public string Title { get; set; }
+
+        List<PhoneTypeEnumModel> PhoneTypeEnumData { get; set; } = new List<PhoneTypeEnumModel>();
+
+        protected override void OnInitialized()
+        {
+            foreach (PhoneType item in Enum.GetValues(typeof(PhoneType)))
+            {
+                if (item != PhoneType.Unknown)
+                    PhoneTypeEnumData.Add(new PhoneTypeEnumModel { DisplayText = item.ToString(), Value = item });
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            Title = FormTitle.BuildTitle(FormMode, "Phone");
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                // TODO: this focuses the element but leaves the cursor at the end
+                //await JsInterop.InvokeVoidAsync("jsfunction.focusElement", "phoneNumber");
+            }
+        }
+
+        internal class PhoneTypeEnumModel
+        {
+            public PhoneType Value { get; set; }
+            public string DisplayText { get; set; }
+        }
+    }
+}
---
+++ Menominee.Client/Components/Phones/PhoneListEditor.razor.cs
@@ -0,0 +1,175 @@
+﻿using CustomerVehicleManagement.Shared.Models.Payables.Invoices.LineItems;
+using Menominee.Common.Enums;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Telerik.Blazor.Components;
+using Telerik.Blazor;
+using CustomerVehicleManagement.Shared.Models.Contactable;
+using Syncfusion.Blazor.Lists;
+using Menominee.Client.Shared;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Menominee.Client.Components.Phones
+{
+    public partial class PhoneListEditor : ComponentBase
+    {
+        [Parameter]
+        public IList<PhoneToWrite> Phones { get; set; }
+
+        [Parameter]
+        public FormMode FormMode { get; set; }
+
+        [Parameter]
+        public PhoneType DefaultPhoneType { get; set; } = PhoneType.Mobile;
+
+        [CascadingParameter]
+        public DialogFactory Dialogs { get; set; }
+
+        private IEnumerable<PhoneToWrite> SelectedPhones { get; set; } = Enumerable.Empty<PhoneToWrite>();
+        private PhoneToWrite SelectedPhone { get; set; }
+        private PhoneToWrite PhoneToModify { get; set; } = null;
+
+        private TelerikGrid<PhoneToWrite> Grid { get; set; }
+
+        private int SelectedPhoneIndex
+        {
+            get => selectedPhoneIndex;
+            set
+            {
+                selectedPhoneIndex = value;
+                CanEditPhone = selectedPhoneIndex >= 0;
+                CanDeletePhone = CanEdit && selectedPhoneIndex >= 0;
+            }
+        }
+
+        private bool CanEdit { get; set; } = false;
+
+        private long phoneIdToSelect { get; set; } = 0;
+        private int selectedPhoneIndex = -1;
+        private bool editDialogVisible = false;
+
+        private FormMode PhoneFormMode { get; set; } = FormMode.Unknown;
+        private bool CanEditPhone { get; set; } = false;
+        private bool CanDeletePhone { get; set; } = false;
+
+        private bool EditDialogVisible
+        {
+            get => editDialogVisible;
+            set
+            {
+                if (value == true)
+                {
+                    if (PhoneFormMode == FormMode.Add)
+                    {
+                        PhoneToModify = new PhoneToWrite();
+                        PhoneToModify.PhoneType = DefaultPhoneType;
+                        if (Phones.Count == 0)
+                            PhoneToModify.IsPrimary = true;
+                    }
+
+                    if (PhoneFormMode == FormMode.Edit || PhoneFormMode == FormMode.View)
+                    {
+                        PhoneToModify = new PhoneToWrite();
+                        CopyPhone(SelectedPhone, PhoneToModify);
+                    }
+                }
+                else
+                {
+                    if (PhoneToModify is not null)
+                        PhoneToModify = null;
+                    PhoneFormMode = FormMode.Unknown;
+                }
+
+                editDialogVisible = value;
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            CanEdit = FormMode == FormMode.Add || FormMode == FormMode.Edit;
+
+            if (Phones?.Count > 0)
+            {
+                if (phoneIdToSelect == 0)
+                    SelectedPhone = Phones.FirstOrDefault();
+
+                SelectedPhoneIndex = Phones.IndexOf(SelectedPhone);
+                SelectedPhones = new List<PhoneToWrite> { SelectedPhone };
+            }
+        }
+
+        private void OnEdit()
+        {
+            if (CanEditPhone)
+            {
+                PhoneFormMode = CanEdit ? FormMode.Edit : FormMode.View;
+                EditDialogVisible = true;
+            }
+        }
+
+        private void OnNew()
+        {
+            PhoneFormMode = FormMode.Add;
+            EditDialogVisible = true;
+        }
+
+        private async Task OnDelete()
+        {
+            if (SelectedPhone is not null
+            && await Dialogs.ConfirmAsync($"Are you sure you want to remove {SelectedPhone.Number.ToString()}?", "Remove Phone"))
+            {
+                Phones.Remove(SelectedPhone);
+                SelectedPhone = Phones.FirstOrDefault();
+                SelectedPhones = new List<PhoneToWrite> { SelectedPhone };
+                SelectedPhoneIndex = Phones.IndexOf(SelectedPhone);
+                Grid.Rebind();
+            }
+        }
+
+        private void OnSaveEdit()
+        {
+            if (PhoneFormMode != FormMode.Add && PhoneFormMode != FormMode.Edit)
+                return;
+
+            if (PhoneFormMode == FormMode.Add)
+            {
+                Phones.Add(PhoneToModify);
+                SelectedPhoneIndex = Phones.IndexOf(PhoneToModify);
+                SelectedPhone = Phones[SelectedPhoneIndex];
+                SelectedPhones = new List<PhoneToWrite> { SelectedPhone };
+                Grid.Rebind();
+            }
+            else if (PhoneFormMode == FormMode.Edit)
+            {
+                CopyPhone(PhoneToModify, Phones[SelectedPhoneIndex]);
+            }
+            EditDialogVisible = false;
+            StateHasChanged();
+        }
+
+        private void OnCancelEdit()
+        {
+            PhoneFormMode = FormMode.Unknown;
+            EditDialogVisible = false;
+        }
+
+        private void OnRowSelected(GridRowClickEventArgs args)
+        {
+            SelectedPhone = args.Item as PhoneToWrite;
+            SelectedPhoneIndex = Phones.IndexOf(SelectedPhone);
+            SelectedPhones = new List<PhoneToWrite> { SelectedPhone };
+        }
+
+        private static void CopyPhone(PhoneToWrite src, PhoneToWrite dst)
+        {
+            dst.Id = src.Id;
+            dst.Number = src.Number;
+            dst.PhoneType = src.PhoneType;
+            dst.IsPrimary = src.IsPrimary;
+        }
+
+    }
+}
