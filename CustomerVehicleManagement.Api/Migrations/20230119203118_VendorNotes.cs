a6502720a55db009bef0e96ce7ee13ea5d8b4b76
David Eaton <DavidE@stocktrac.com>
2023-01-19 15:33:17 -0500

    Add VendorNotes migration

---
+++ CustomerVehicleManagement.Api/Migrations/20230119203118_VendorNotes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerVehicleManagement.Api.Migrations
+{
+    public partial class VendorNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                schema: "dbo",
+                table: "Vendor",
+                newName: "Notes");
+
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                schema: "dbo",
+                table: "Organization",
+                newName: "Notes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                schema: "dbo",
+                table: "Vendor",
+                newName: "Note");
+
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                schema: "dbo",
+                table: "Organization",
+                newName: "Note");
+        }
+    }
+}
