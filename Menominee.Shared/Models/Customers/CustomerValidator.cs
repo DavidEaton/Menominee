5e76d4db130d3b95af90405f26d495fe2063c733
Matthew Palacio <mpalacio@catalyte.io>
2023-07-14 16:53:05 +0000

    Merged in MEN_308_customer_validator (pull request #34)
    Create a validator for customer entity that validates the person, organization, and creates the new customer based upon their type
    
    remove unused comments and using statements
    
    update the When methods inside of the customer validator to set the validator based on the customer entity type equaling either person or organization rather than not null
    
    Approved-by: Alan Craker
    Approved-by: David Eaton

---
+++ Menominee.Shared/Models/Customers/CustomerValidator.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Menominee.Common.Enums;
+using Menominee.Common.ValueObjects;
+using Menominee.Domain.Entities;
+using Menominee.Shared.Models.Organizations;
+using Menominee.Shared.Models.Persons;
+
+namespace Menominee.Shared.Models.Customers
+{
+    public class CustomerValidator : AbstractValidator<CustomerToWrite>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(customer => customer.EntityType)
+                .IsInEnum();
+
+            RuleFor(customer => customer.CustomerType)
+                .IsInEnum();
+
+            RuleFor(customer => customer.Person)
+                .SetValidator(new PersonValidator())
+                .When(customer => customer.EntityType == EntityType.Person);
+
+            RuleFor(customer => customer.Organization)
+                .SetValidator(new OrganizationValidator())
+                .When(customer => customer.EntityType == EntityType.Organization);
+
+            RuleFor(customer => customer)
+                 .MustBeEntity(customer =>
+                     Customer.Create(
+                         Person.Create(
+                             PersonName.Create(
+                                 customer.Person.Name.LastName, customer.Person.Name.FirstName).Value,
+                                 customer.Person.Gender,
+                                 customer.Person.Notes).Value,
+                             customer.CustomerType))
+                 .When(customer => customer.EntityType == EntityType.Person);
+
+            RuleFor(customer => customer)
+                .MustBeEntity(customer =>
+                    Customer.Create(
+                        Organization.Create(
+                            OrganizationName.Create(customer.Organization.Name).Value, customer.Organization.Notes).Value,
+                            customer.CustomerType))
+                .When(customer => customer.EntityType == EntityType.Organization);
+        }
+    }
+}
