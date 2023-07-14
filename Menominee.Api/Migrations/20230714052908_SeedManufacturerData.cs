da94f33ba6682698293e7fbd2e09ef00ac6e9440
Matthew Palacio <mpalacio@catalyte.io>
2023-07-14 19:27:52 +0000

    Merged in MEN-561-add-the-default-manufacturers-to-manufacturer-table (pull request #36)
    Manually insert default data during migration instead of seeding data during database configuration
    
    Approved-by: David Eaton
    Approved-by: Randi Blankenship

---
+++ Menominee.Api/Migrations/20230714052908_SeedManufacturerData.cs
@@ -0,0 +1,31 @@
+﻿using Menominee.Domain.Entities.Inventory;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Menominee.Api.Migrations
+{
+    public partial class SeedManufacturerData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Manufacturer",
+                schema: "dbo",
+                columns: new[]
+                {
+                    nameof(Manufacturer.Name),
+                    nameof(Manufacturer.Prefix),
+                    nameof(Manufacturer.Code)
+                },
+                values: new object[,]
+                {
+                    { "Custom", "I", "0" },
+                    { "Miscellaneous", "X", "1" },
+                    { "Custom - Stocked", "CS", "2" },
+                    { "Package", "PKG", "3" }
+                });
+        }
+
+    }
+}
