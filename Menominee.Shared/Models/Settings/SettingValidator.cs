a46feed478313ab6e6710379ca2c445002edaaa5
Randi Blankenship <rblankenship@catalyte.io>
2023-08-28 13:42:49 +0000

    Merged in MEN-870-settings-integration-test-fails-during-run-until-failure (pull request #69) Remove NotEmpty() and replace with NotNull() on rules for enums in setting validator Remove redundant rules in settings validator
    Approved-by: David Eaton
    Approved-by: Matthew Palacio

--- Menominee.Shared/Models/Settings/SettingValidator.cs
+++ Menominee.Shared/Models/Settings/SettingValidator.cs
@@ -5,24 +5,8 @@ namespace Menominee.Shared.Models.Settings;
 
 public class SettingValidator : AbstractValidator<SettingToWrite>
 {
-    private const string notEmptyMessage = "Setting must not be empty";
-
     public SettingValidator()
     {
-        RuleFor(setting => setting.SettingName)
-            .NotEmpty()
-            .WithMessage(notEmptyMessage)
-            .IsInEnum();
-
-        RuleFor(setting => setting.SettingGroup)
-            .NotEmpty()
-            .WithMessage(notEmptyMessage)
-            .IsInEnum();
-
-        RuleFor(setting => setting.SettingValue)
-            .NotNull()
-            .WithMessage(notEmptyMessage);
-
         RuleFor(setting => setting)
             .MustBeEntity(setting => ConfigurationSetting.Create(
                 setting.SettingName,
