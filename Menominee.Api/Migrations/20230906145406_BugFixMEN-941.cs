21c12eb38a872e90504d2d7466ee0d92bdd38629
Matthew Palacio <mpalacio@catalyte.io>
2023-09-06 15:17:18 +0000

    Merged in MEN-941-running-command-add-migration-on-master-has-changes-on-repair-order-item-table (pull request #76)
    MEN-941 Add new migration that renames RepairOrderItems table to RepairOrderItem
    
    * MEN-941 add new migration that renames RepairOrderItems table to RepairOrderItem
    
    MEN-941 remove RenameRepairOrderItemsTableToRepairOrderItem migration and add a blank migration to generate a new hash for the model snapshot
    
    
    Approved-by: Alan Craker
    Approved-by: Randi Blankenship

---
+++ Menominee.Api/Migrations/20230906145406_BugFixMEN-941.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Menominee.Api.Migrations
+{
+    public partial class BugFixMEN941 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Generate a fresh hash for the snapshot
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
