821e7220c13b5f63516a3d4c9e5299d743b94736
David Eaton <davide@stocktrac.com>
2023-10-04 18:49:48 +0000

    Merged in feature/MEN-1026-add-vehiclehelper-method (pull request #89)
    Add VehicleHelper.ConvertWriteToReadDto method
    
    
    Approved-by: Alan Craker

--- Menominee.Shared/Models/Vehicles/VehicleHelper.cs
+++ Menominee.Shared/Models/Vehicles/VehicleHelper.cs
@@ -6,6 +6,26 @@ namespace Menominee.Shared.Models.Vehicles
 {
     public class VehicleHelper
     {
+        public static VehicleToRead ConvertWriteToReadDto(VehicleToWrite vehicle)
+        {
+            return vehicle is null
+                ? new()
+                : new()
+                {
+                    Id = vehicle.Id,
+                    Make = vehicle.Make,
+                    Model = vehicle.Model,
+                    Year = vehicle.Year,
+                    VIN = vehicle.VIN,
+                    Plate = vehicle.Plate,
+                    PlateStateProvince = vehicle.PlateStateProvince,
+                    UnitNumber = vehicle.UnitNumber,
+                    Color = vehicle.Color,
+                    Active = vehicle.Active,
+                    NonTraditionalVehicle = vehicle.NonTraditionalVehicle
+                };
+        }
+
         public static VehicleToRead ConvertToReadDto(Vehicle vehicle)
         {
             return vehicle is null
