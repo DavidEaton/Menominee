b5964f60b135fce388824a5e07bbc6faf7eb13cd
David Eaton <davidzaeaton@outlook.com>
2026-06-29 17:38:51 -0400

    Upgrade API project from .net 6 to 10. Refactor MSGraphUserService methods to throw NotImplementedExceptions; update target framework and package references in project files; clean up Program.cs and nuget.config for clarity.

--- Menominee.Api/MSGraphConfiguration.cs
+++ Menominee.Api/MSGraphConfiguration.cs
@@ -11,16 +11,16 @@ namespace Menominee.Api
         //Called in Startup - services.ConfigureGraphComponent(configuration)
         public static IServiceCollection ConfigureMSGraphComponent(this IServiceCollection services, IConfiguration configuration)
         {
-            //Look at appsettings.Development.json | https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.1
+            // //Look at appsettings.Development.json | https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.1
 
-            var graphConfig = configuration.GetSection("MSGraphConfig").Get<GraphConfiguration>();
+            // var graphConfig = configuration.GetSection("MSGraphConfig").Get<GraphConfiguration>();
 
-            // Initialize the client credential auth provider
-            var scopes = new[] { "https://graph.microsoft.com/.default" };
-            var clientSecretCredential = new ClientSecretCredential(graphConfig.TenantId, graphConfig.AppId, graphConfig.ClientSecret);
+            // // Initialize the client credential auth provider
+            // var scopes = new[] { "https://graph.microsoft.com/.default" };
+            // var clientSecretCredential = new ClientSecretCredential(graphConfig.TenantId, graphConfig.AppId, graphConfig.ClientSecret);
 
-            //you can use a single client instance for the lifetime of the application
-            services.AddSingleton(provider => new GraphServiceClient(clientSecretCredential, scopes));
+            // //you can use a single client instance for the lifetime of the application
+            // services.AddSingleton(provider => new GraphServiceClient(clientSecretCredential, scopes));
 
             return services;
         }
